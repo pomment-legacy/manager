8b297f2bac1338f79ce1c69d4beaf071efcfade1
tcdw <tcdw2011@gmail.com>
2020-06-05 12:19:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+    <router-view/>
+    </div>
+</template>
+
+<style lang="scss">
+body {
+    font-family:
+    "lucida grande",
+    "lucida sans unicode",
+    "Helvetica Neue",
+    Tahoma,
+    "PingFang SC",
+    "Hiragino Sans GB",
+    "Source Han Sans CN Normal",
+    "Heiti SC",
+    "Microsoft YaHei",
+    "WenQuanYi Micro Hei",
+    SimSun,
+    sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <HelloWorld msg="土豆大王真是太伟大了！！！"/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        HelloWorld,
+    },
+};
+</script>
