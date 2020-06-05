2c67a4f4826d4f11f60551dd8d18bcad52278954
tcdw <tcdw2011@gmail.com>
2020-06-05 17:30:09 +0800

    设置变更

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+
+module.exports = {
+    chainWebpack: (config) => {
+        config
+            .plugin('html')
+            .tap((args) => {
+                args[0].title = 'Pomment Manager';
+                return args;
+            });
+    },
+};
