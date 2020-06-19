28ec5684ab75b9ac2c3246571be49e51b4593142
tcdw <tcdw2011@gmail.com>
2020-06-19 12:20:50 +0800

    评论详情页面

--- src/views/List/Posts.vue
+++ src/views/List/Posts.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="section-outer">
         <header>
-            <ul class="breadcrumb">
-                <li><router-link to="/list/threads">Threads</router-link></li>
-                <li>{{ thread.attr.title }}</li>
-            </ul>
             <h1 class="title">{{ thread.attr.title }}</h1>
+            <ul class="thread-option">
+                <li><router-link to="/list/threads">Back</router-link></li>
+                <li><router-link to="meta">Edit Meta</router-link></li>
+                <li>Lock</li>
+                <li><a :href="thread.url" target="_blank">Original</a></li>
+            </ul>
         </header>
+        <section class="status">
+            {{ thread.attr.amount }} Replies
+        </section>
     </div>
 </template>
 
@@ -14,18 +19,30 @@
 .section-outer {
     --bg: #282828;
     --shadow: rgba(0, 0, 0, 0.1) 0.5rem 0.5rem 1rem 1rem;
+    --border: #484848;
+    --actionBar: rgba(255, 255, 255, 0.5);
     @media screen and (prefers-color-scheme: light) {
         --bg: #fff;
         --shadow: rgba(0, 0, 0, .09) 0.5rem 0.5rem 1rem 0.25rem;
+        --border: #b9b9b9;
+        --actionBar: rgba(0, 0, 0, 0.3);
     }
 
     background-color: var(--bg);
     border-radius: 0.5rem;
     box-shadow: var(--shadow);
-    padding: 0 0.75em;
-    ul.breadcrumb {
+    padding: 0 0.8rem;
+    h1.title {
+        $lineHeight: 2.2rem;
+        line-height: $lineHeight;
         margin: 0;
-        padding: 1em 0;
+        padding: 1.2rem - (($lineHeight - 1rem) / 2) 0;
+        font-size: 1.4em;
+        border-bottom: 1px var(--border) dashed;
+    }
+    ul.thread-option {
+        margin: 0;
+        padding: 0.8rem 0;
         font-size: 0.875rem;
         li {
             margin: 0;
@@ -34,12 +51,13 @@
             list-style: none;
             display: inline-block;
             &:after {
-                content: '•';
+                content: '|';
                 position: absolute;
                 width: 1.6em;
                 height: 1em;
                 line-height: 1em;
                 text-align: center;
+                color: var(--actionBar);
             }
             &:last-child {
                 padding-right: 0;
@@ -48,11 +66,6 @@
                 }
             }
         }
-    }
-    h1.title {
-        margin: 0;
-        padding: 0.8rem 0;
-        font-size: 1.6em;
     }
 }
 </style>
